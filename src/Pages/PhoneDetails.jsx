5f63df1a6db342e5d155677a56794d52bcfb11cc
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-08-15 08:57:13 +0100

    Dynamic Routing done

--- src/Pages/PhoneDetails.jsx
+++ src/Pages/PhoneDetails.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { useLoaderData, useParams } from "react-router";
-
 const PhoneDetails = () => {
   const data = useLoaderData();
-  const params = useParams();
-  const singlePhone = data.find(phone=>phone.id===parseInt(id))
-  console.log(params);
+  const { id } = useParams();
+  const singlePhone = data.find((phone) => phone.id === parseInt(id));
+  console.log(singlePhone);
+  const { name, brand, image } = singlePhone;
+
   return (
     <div>
-      <h5>Phone Details</h5>
+      <h1 className="font-extrabold text-4xl">{name}</h1>
+      <h2 className="font-bold text-2xl">{brand}</h2>
+      <img className="w-full" src={image} alt="" />
     </div>
   );
 };
-
 export default PhoneDetails;
